79279d4bc777cd52adf63373883e586e32b2f7de
nursultan <nursultan@cloudcoure.com>
2017-06-12 11:30:07 +0600

    PLA-4022: refactored

--- js/audit/audit.js
+++ js/audit/audit.js
@@ -178,7 +178,7 @@ window.Audit = (function (Resource, AuditRender) {
                     alert.id = violationKey;
                     alert.resource = resource;
                     alert.timestamp = timestamp;
-                    if (violations[violationKey].inputs)
+                    if (violations[violationKey] && violations[violationKey].inputs)
                         alert.metas = AuditUtils.getRuleMetasCis(violations[violationKey].inputs);
                     else
                         alert.metas = AuditUtils.getRuleMetasCis(rowData);

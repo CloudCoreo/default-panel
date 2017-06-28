b0bb8df21c20d8954a0febef2448129af8da8786
smattes <mattes@nixnux.org>
2017-06-28 11:54:52 -0700

    dev to master 28Jun2017 (#175)
    * PLA-3991: Fixed constants for map
    
    * [PLA-3906] send request is not defined
    
    * PLA-3991: Moved the check above
    
    * PLA-4004: Added sort by NIST id
    
    * PLA-4004: Changed labels for meta attributes
    
    * PLA-4004: Uncommented necessary part of code
    
    * PLA-4004: Some fixes for rules
    
    * [PLA-3906] send request is not defined
    
    * PLA-4004: Added violation cloning by nistId logic
    
    * PLA-4004: Added deep copying for alert object
    
    * PLA-4022: fixed trouble with some meta tags are not showing up in panel cards. This meta tags don't exist in rowdata.violations[violation_key] object
    
    * PLA-4022: refactored
    
    * PLA-4022: refactored
    
    * PLA-4022: refactored
    
    * PLA-4004: Added suppressed violation logic for sorting by nist id
    
    * PLA-4004: Fixed violating objects popup
    
    * PLA-4004: Fixed variable name
    
    * PLA-3932: Added unbinding for event handlers
    
    * [pla-3906] send request is not defined
    
    * PLA-4004: Changed headers and counters
    
    * PLA-4004: Changed class name
    
    * PLA-4004: Changed class name again
    
    * PLA-4004: Removed rules without cis id and nist id from list
    
    * PLA-4017: Changed logic for disabled rules on panel
    
    * PLA-4017: Changed gitignore
    
    * PLA-4017: Added method to unbind existed handlers
    
    * PLA-4017: Fixed query parser
    
    * PLA-4004: Fixed headers in pie chart
    
    * PLA-4004: Fixes for pie chart
    
    * PLA-4017: Changed logic of filling disabled violations
    
    * PLA-4017: Some fixes to new logic of disabled violations
    
    * PLA-4017: Fixes for pie chart in case of "All clear"
    
    * Create README.md
    
    add build status
    
    * Pla 4094 panel not rendering (#170)
    
    * PLA-4094: add debugging statements
    
    * PLA-4094: fix debug statements
    
    * PLA-4094: use alert instead of console.log
    
    * PLA-4094: use temp div for debug info
    
    * PLA-4094: use temp div for debug info
    
    * PLA-4094: guard against nil
    
    * PLA-4094: properly guard against nill queryString; remove debugging
    
    * fixed typo for no violations
    
    * fixed typos for no violations
    
    * PLA-4108: Fixed sorting by nist id
    
    * PLA-4103: added 'inventory' to violation_levels list
    
    * PLA-4108: Changed params for method that refresh click handlers
    
    * PLA-4106: Added check for region in alert

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+    window.isLocalRun = typeof ccThisCont === 'undefined';
+
     var auditData;
     var deployData;
     var map;
@@ -11,8 +13,26 @@ $(document).ready(function () {
     var currentView;
     var counter = 0;
 
-    var templates = Constants.TEMPLATES;
+    var templates = Constants.TEMPLATE_IDS;
 
+    function parseQueries(queryString) {
+        var parsedQueries = {};
+        var queries = [];
+
+        if (!queryString) return parsedQueries;
+
+        queries = queryString.split('&');
+
+        queries.forEach(function(query) {
+            query = query.split('=');
+            parsedQueries[query[0]] = query[1];
+        });
+
+        return parsedQueries;
+    }
+
+    var queryString = window.location.href.split('?');
+    window.parsedQueries = parseQueries(queryString[1]);
 
     function getRegion(resource) {
         function getRegionValue() {
@@ -235,26 +255,13 @@ $(document).ready(function () {
         setExecutionStatusMessage(data);
     }
 
-    function parseQueries(queryString) {
-        var queries = queryString.split('&');
-        var parsedQueries = {};
-
-        queries.forEach(function(query) {
-           query = query.split('=');
-           parsedQueries[query[0]] = query[1];
-        });
-
-        return parsedQueries;
-    }
-
-    if (typeof ccThisCont === 'undefined') {
-        var queryString = window.location.href.split('?')[1];
-        var parsedQueries = parseQueries(queryString);
-        if (!parsedQueries.tmpfile) {
+    if (window.isLocalRun) {
+        if (!window.parsedQueries.tmpfile) {
             console.log('Please add tmpFile in url params', 'expamle: ?tmpfile=./tmp-data/tmp0.json');
             return;
         }
-        d3.json(parsedQueries.tmpfile, function (data) {
+
+        d3.json(window.parsedQueries.tmpfile, function (data) {
             init(data, true);
             // emulateCcThisUpdate(data)
         });

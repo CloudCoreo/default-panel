a5f612a4074d89f2f2653fe14b81d628f0bc0f4b
omurbek.jk@gmail.com <omurbek.jk@gmail.com>
2017-08-29 15:51:02 +0600

    PLA-4549: shows animation when plan is not completed

--- js/audit/audit.js
+++ js/audit/audit.js
@@ -16,7 +16,7 @@ window.Audit = (function (Resource, AuditRender) {
 
 
     function isRuleRunner(resourceType) {
-        return Constants.RULE_RUNNERS[resourceType] || 
+        return Constants.RULE_RUNNERS[resourceType] ||
             resourceType.indexOf(Constants.RULE_RUNNERS.SUFFIX) !== -1; // support for deprecated rule runners
     }
 
@@ -192,7 +192,7 @@ window.Audit = (function (Resource, AuditRender) {
             auditRender.setChartHeaderText(uiTexts.CHART_HEADER.CLOUD_OBJECTS, sortKey);
             return;
         }
-        if(!hasOld){
+        if (!hasOld) {
             AuditUI.showResourcesAreBeingLoadedMessage();
         }
     }
@@ -328,7 +328,7 @@ window.Audit = (function (Resource, AuditRender) {
         };
 
         if (ccThisData.auditResults && Object.keys(ccThisData.auditResults).length) {
-            Object.keys(ccThisData.auditResults).forEach( function (reportId) {
+            Object.keys(ccThisData.auditResults).forEach(function (reportId) {
                 reorganizeReportData(ccThisData.auditResults[reportId], reportId, undefined, violations);
             });
             callback();
@@ -390,7 +390,7 @@ window.Audit = (function (Resource, AuditRender) {
             }
         });
 
-        if (!executionIsFinished && !hasOld){
+        if (!executionIsFinished && !hasOld) {
             AuditUI.showResourcesAreBeingLoadedMessage();
             alerts = undefined;
             callback(sortKey);
@@ -646,7 +646,7 @@ window.Audit = (function (Resource, AuditRender) {
     }
 
     function isOldResource(resource) {
-       return resource.runId !== ccThisData.runId;
+        return resource.runId !== ccThisData.runId;
     }
 
     function init(sortKey, callback) {
@@ -671,6 +671,10 @@ window.Audit = (function (Resource, AuditRender) {
         executionIsFinished = isCompleted || (isPlanned && !isStatusOK);
 
         hasExecutionError = ccThisData.engineStatus === Constants.ENGINE_STATUSES.EXECUTION_ERROR;
+
+        if (!executionIsFinished && !hasOld) {
+            AuditUI.showResourcesAreBeingLoadedMessage();
+        }
 
         var initRender = function (sortKey) {
             if (alerts) {
@@ -706,7 +710,7 @@ window.Audit = (function (Resource, AuditRender) {
             return;
         }
 
-        if (ccThisData.runId === data.runId && data.engineState !== Constants.ENGINE_STATES.COMPLETED){
+        if (ccThisData.runId === data.runId && data.engineState !== Constants.ENGINE_STATES.COMPLETED) {
             callback();
             return;
         }
